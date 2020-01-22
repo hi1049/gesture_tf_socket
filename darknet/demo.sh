6824a8b411e57c299569707225ca83e537fb339f
hi1049 <lah2000@naver.com>
2020-01-22 17:48:03 +0900

    init

---
+++ darknet/demo.sh
@@ -0,0 +1,4 @@
+#./darknet detector demo cfg/coco.data cfg/yolov3.cfg cfg/yolov3.weights
+./darknet detector demo cfg/coco.data cfg/yolov3.cfg cfg/yolov3.weights demo.mp4
+#./darknet detector demo cfg/coco.data cfg/yolov2.cfg cfg/yolov2.weights demo.mp4
+
